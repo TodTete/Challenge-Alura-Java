3c3d4bcfaeda5e651d19d5289318894a9a3c886e
TodTete <vallejoricardo3@gmail.com>
2025-12-08 19:10:16 -0600

    New File #6

--- src/main/java/com/example/challenge/ricardovs/dto/GutendexAuthorDto.java
+++ src/main/java/com/example/challenge/ricardovs/dto/GutendexAuthorDto.java
@@ -1,4 +1,26 @@
 package com.example.challenge.ricardovs.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GutendexAuthorDto {
+    private String name;
+    private Integer birth_year;
+    private Integer death_year;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Integer getBirth_year() { return birth_year; }
+    public void setBirth_year(Integer birth_year) { this.birth_year = birth_year; }
+    public Integer getDeath_year() { return death_year; }
+    public void setDeath_year(Integer death_year) { this.death_year = death_year; }
+
+    @Override
+    public String toString() {
+        return "GutendexAuthorDto{" +
+                "name='" + name + '\'' +
+                ", birth_year=" + birth_year +
+                ", death_year=" + death_year +
+                '}';
+    }
 }
