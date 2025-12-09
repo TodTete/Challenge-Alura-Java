423a3015bbf93555449b740a84c0100c2ab627f4
TodTete <vallejoricardo3@gmail.com>
2025-12-08 19:13:28 -0600

    New File #11

--- src/main/java/com/example/challenge/ricardovs/repository/BookRepository.java
+++ src/main/java/com/example/challenge/ricardovs/repository/BookRepository.java
@@ -1,4 +1,11 @@
 package com.example.challenge.ricardovs.repository;
 
-public class BookRepository {
+import com.example.challenge.ricardovs.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByLanguage(String language);
 }
