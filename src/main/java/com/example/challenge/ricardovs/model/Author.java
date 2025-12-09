86c28cef350006fd5cbad034add3c05be0cb1d75
TodTete <vallejoricardo3@gmail.com>
2025-12-08 19:11:35 -0600

    New File #8

--- src/main/java/com/example/challenge/ricardovs/model/Author.java
+++ src/main/java/com/example/challenge/ricardovs/model/Author.java
@@ -1,4 +1,39 @@
 package com.example.challenge.ricardovs.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authors")
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer birthYear;
+    private Integer deathYear;
+
+    // Constructors
+    public Author() {}
+    public Author(String name, Integer birthYear, Integer deathYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.deathYear = deathYear;
+    }
+
+    // getters y setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Integer getBirthYear() { return birthYear; }
+    public void setBirthYear(Integer birthYear) { this.birthYear = birthYear; }
+    public Integer getDeathYear() { return deathYear; }
+    public void setDeathYear(Integer deathYear) { this.deathYear = deathYear; }
+
+    @Override
+    public String toString() {
+        return "Author{id=" + id + ", name='" + name + "', birthYear=" + birthYear + ", deathYear=" + deathYear + '}';
+    }
 }
